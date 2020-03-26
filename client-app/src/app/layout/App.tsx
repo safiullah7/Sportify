77a962f741c64f21c58cca9a82b4744a06c036f2
safiullah7 <iamsafiullah@gmail.com>
2020-03-26 07:47:49 +0500

    CRUD activity and some client-app structure

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
 import axios from 'axios';
+import { IActivity } from '../models/activity';
 
-class App extends Component {
-  state = {
-    values: []
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component<{}, IState> {
+  readonly state: IState = {
+    activities: []
   };
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
+    axios.get<IActivity[]>('http://localhost:5000/api/activities')
           .then((response) => {
             this.setState({
-              values: response.data
+              activities: response.data
             });
           });
   }
@@ -23,8 +27,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-            {this.state.values.map((value: any) => (
-              <List.Item key={value.id}>{value.name}</List.Item>
+            {this.state.activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
             ))}
         </List>
       </div>
