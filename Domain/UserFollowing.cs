f56ad8a99a3d2121b1bf0d8d741bf1427f2c085e
safiullah7 <iamsafiullah@gmail.com>
2020-05-02 17:12:35 +0500

    Following a user: Add Handler

--- Domain/UserFollowing.cs
+++ Domain/UserFollowing.cs
@@ -2,6 +2,12 @@ namespace Domain
 {
     public class UserFollowing
     {
+        /*
+            1. An observer is one who follows a user. Target is the one who has followers
+            2. Added Followers and Followings props in AppUser
+            3. DataContext updated with builder code as well
+            4. Handlers
+        */
         public string ObserverId { get; set; }
         public virtual AppUser Observer { get; set; }
         public string TargetId { get; set; }
